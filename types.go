548a355fab89c1743f657819150ed886263aec1d
Alex Zylman <alex.zylman@getclever.com>
2014-07-15 13:41:17 -0700

    add packet types

---
+++ types.go
@@ -0,0 +1,39 @@
+package gearman
+
+const (
+	CanDo           = 1
+	CantDo          = 1
+	ResetAbilities  = 3
+	PreSleep        = 4
+	NoOp            = 6
+	SubmitJob       = 7
+	JobCreated      = 8
+	GrabJob         = 9
+	NoJob           = 10
+	JobAssign       = 11
+	WorkStatus      = 12
+	WorkComplete    = 13
+	WorkFail        = 14
+	GetStatus       = 15
+	EchoReq         = 16
+	EchoRes         = 17
+	SubmitJobBg     = 18
+	Error           = 19
+	StatusRes       = 20
+	SubmitJobHigh   = 21
+	SetClientId     = 22
+	CanDoTimeout    = 23
+	AllYours        = 24
+	WorkException   = 25
+	OptionReq       = 26
+	OptionRes       = 27
+	WorkData        = 28
+	WorkWarning     = 29
+	GrabJobUniq     = 30
+	JobAssignUniq   = 31
+	SubmitJobHighBg = 32
+	SubmitJobLow    = 33
+	SubmitJobLowBg  = 34
+	SubmitJobSched  = 35
+	SubmitJobEpoch  = 36
+)
