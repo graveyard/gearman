1a55a2216d60fa041c4c1b28cf22020bb1a380b4
Jeffrey Nelson <jeff.nelson@clever.com>
2014-07-15 12:02:46 -0700

    Implemented scanner to split TCP stream on packets.

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -6,11 +6,25 @@ import (
 
 func needMoreData() (int, []byte, error) { return 0, nil, nil }
 
+const HeaderSize = 12
+
 func New(r io.Reader) *bufio.Scanner {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(func(data []byte, atEOF bool) (int, []byte, error) {
-		// TODO
-		return 0, nil, nil
+		if len(data) < HeaderSize {
+			needMoreData()
+		}
+
+		var size int32
+		if err := binary.Read(bytes.NewBuffer(data[8:4]), binary.BigEndian, &size); err != nil {
+			return 0, nil, err
+		}
+
+		if len(data) < HeaderSize+int(size) {
+			return needMoreData()
+		}
+
+		return int(size), data[0 : HeaderSize+size], nil
 	})
 	return scanner
 }
